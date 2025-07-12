a9479b90bcd8a2c83ed68503beff89e65488a7f1
Jotaefe <juanfranruiz98@gmail.com>
2025-07-12 11:35:39 +0200

    clean up

--- server/src/test.ts
+++
@@ -1,9 +0,0 @@
-// Test file to verify TypeScript configuration
-import express from "express";
-import { Server } from "socket.io";
-import { MongoClient } from "mongodb";
-
-const app = express();
-console.log("TypeScript configuration test passed!");
-
-export default app;
